06f7a5aadf052cc4f38233f603e7501bac3092ae
Otto Neres <68795143+ottoneres@users.noreply.github.com>
2021-02-17 21:42:01 -0300

    Update cnpj.py
    Remoção de comentários específicos da ferramenta utilizada para desenvolvimento.

--- cnpj.py
+++ cnpj.py
@@ -1,28 +1,3 @@
-# def valida_cnpj(cnpj):
-#     cnpj = remove_caracteres(cnpj)
-#     if cnpj:
-#         sequencia = cnpj == str(cnpj[0]) * len(cnpj)
-#     elif len(cnpj) < 14:
-#         return print(f'O CNPJ {cnpj} não é válido!')
-#     else:
-#         return print(f'O CNPJ {cnpj} não é válido!')
-#
-#     if not cnpj.isnumeric() or sequencia:
-#         return print(f'O CNPJ {cnpj} não é válido!')
-#
-#     novo_cnpj = calcular_digitos(cnpj)
-#     if novo_cnpj == cnpj:
-#         return print(f'CNPJ válido')
-#     else:
-#         return print(f'CNPJ Inválido!')
-
-
-# Você pode reformatar o código pressionando Ctrl Alt L
-# def remove_caracteres(cnpj):
-#     return re.sub(r'[^0-9]', '', cnpj)  # expressão regular: ^ - diferente de | 0 a 9.
-#     # Tudo que for diferente de 0 a 9 vai ser substituído por vazio.
-
-
 def calcular_digitos(cnpj):
     reverso = 5
     total = 0
